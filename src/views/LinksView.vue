c4a7bca1020f8bfded58c848f51485a78d9e9efa
longyu <2571778427@qq.com>
2022-12-04 18:27:46 +0800

    feat : inital friend link page

--- src/views/LinksView.vue
+++ src/views/LinksView.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>
-    <h1>友情链接</h1>
+  <div class="container">
+    <div class="page-friends">
+      <ul class="readers-list clearfix">
+        <li class="animated" style="visibility: visible">
+          <a
+            rel="project"
+            title="ToaruNet Project"
+            target="_blank"
+            href="https://toarunet.com"
+            ><div>ToaruNet</div>
+            <div>某系列作品粉丝网站,由星之相位运营</div></a
+          ><br />
+          <a
+            rel="colleague"
+            title="mashiro blog"
+            target="_blank"
+            href="https://mashiro.pub"
+            ><div>Mashiro的博客</div>
+            <div>Happy Coding , Happy Life</div></a
+          >
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
