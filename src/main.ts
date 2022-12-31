c65b7c4772d970a65cae360ce5e5e02334e41cca
guoguo909 <3108426389@qq.com>
2022-12-31 09:43:30 +0800

    增加了css切换的功能

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,18 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 
+var ccss=getCookie(css);
+var css;
+switch(ccss)//css切换器
+{
+  case "null":
+    css=import('@stylesmain.scss');
+    break;
+  case "main":
+    css=import('@stylesmain.scss');
+    break;
+}
+
 import './assets/main.scss';
 
 import VueLazyLoad from 'vue3-lazyload';
@@ -15,3 +27,17 @@ app.use(createPinia());
 app.use(router);
 
 app.mount('#app');
+
+function SetCookie(name: String, value: string) {//设置Cookie
+  document.cookie = name + '=' + value;
+}
+function getCookie(name: string) {//读取Cookie
+  var arr = document.cookie.match(
+    new RegExp('(^| )' + name + '=([^;]*)(;|$)')
+  );
+  if (arr != null) {
+    return arr[2];
+  } else {
+    return 'null';
+  }
+}
