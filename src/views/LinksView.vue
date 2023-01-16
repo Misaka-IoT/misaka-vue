a73cb8cb49e0155a70b44210f9eca1b9887e0251
longyu12345 <litong4576@gmail.com>
2023-01-16 15:06:27 +0800

    add link

--- src/views/LinksView.vue
+++ src/views/LinksView.vue
@@ -8,9 +8,10 @@
             title="ToaruNet Project"
             target="_blank"
             href="https://toarunet.com"
-            ><div>ToaruNet</div>
-            <div>某系列作品粉丝网站,由星之相位运营</div></a
           >
+            <div>ToaruNet</div>
+            <div>某系列作品粉丝网站,由星之相位运营</div>
+          </a>
         </li>
 
         <li class="animated" style="visibility: visible">
@@ -19,58 +20,67 @@
             title="mashiro blog"
             target="_blank"
             href="https://mashiro.pub"
-            ><div>Mashiro的博客</div>
-            <div>Happy Coding , Happy Life</div></a
           >
+            <div>Mashiro的博客</div>
+            <div>Happy Coding , Happy Life</div>
+          </a>
         </li>
-        
 
         <li class="animated" style="visibility: visible">
-
           <a
             title="misaka-fans"
             target="_blank"
             href="https://static.misaka-mikoto.jp"
-            ><div>静态网站</div>
-            <div>本网站的上一个版本，使用静态页面构建</div></a
           >
-</li>
-<li class="animated" style="visibility: visible">
-
+            <div>静态网站</div>
+            <div>本网站的上一个版本，使用静态页面构建</div>
+          </a>
+        </li>
+        <li class="animated" style="visibility: visible">
           <a
             title="LiangBurs' Blog"
             target="_blank"
             href="https://blog.cnmobile.link"
-            ><div>LiangBurs' Blog</div>
-            <div>May all the beauty be blessed. —— 《Moon Halo》</div></a
           >
-</li>
-<li class="animated" style="visibility: visible">
+            <div>LiangBurs' Blog</div>
+            <div>May all the beauty be blessed. —— 《Moon Halo》</div>
+          </a>
+        </li>
+        <li class="animated" style="visibility: visible">
+          <a title="倾丞の小窝" target="_blank" href="https://blog.qcmoe.com">
+            <div>倾丞の小窝</div>
+            <div>倾丞的小窝,记录生活,分享乐趣</div>
+          </a>
+        </li>
 
-          <a title="倾丞の小窝" target="_blank" href="https://blog.qcmoe.com"
-            ><div>倾丞の小窝</div>
-            <div>倾丞的小窝,记录生活,分享乐趣</div></a
-          >
-</li>
+        <li class="animated" style="visibility: visible">
+          <a title="夜灭の小窝" target="_blank" href="https://yujie.pro">
+            <div>夜灭の小窝</div>
+            <div>夜灭的小窝，谈天说地热爱二次元且会写bug</div>
+          </a>
+        </li>
 
-<li class="animated" style="visibility: visible">
-
-          <a title="夜灭の小窝" target="_blank" href="https://yujie.pro"
-            ><div>夜灭の小窝</div>
-            <div>夜灭的小窝，谈天说地热爱二次元且会写bug</div></a
-          >
-</li>
-
-<li class="animated" style="visibility: visible">
-
+        <li class="animated" style="visibility: visible">
           <a
             title="233355607的生草小站"
             target="_blank"
             href="https://www.233355607.xyz"
-            ><div>233355607的生草小站</div>
-            <div>草还是草</div></a
           >
-       </li>
+            <div>233355607的生草小站</div>
+            <div>草还是草</div>
+          </a>
+        </li>
+
+        <li class="animated" style="visibility: visible">
+          <a
+            title="萌又萌"
+            target="_blank"
+            href="https://moeu.moe"
+          >
+            <div>萌又萌</div>
+            <div>API Ready！</div>
+          </a>
+        </li>
       </ul>
     </div>
   </div>
@@ -80,13 +90,14 @@
 .container {
   height: 75vh;
 
-  .readers-list{
+  .readers-list {
     height: 75vh;
-    
+
     display: flex;
     flex-flow: column wrap;
     justify-content: flex-start;
-    li{
+
+    li {
       max-width: 450px;
       margin-left: 20px;
     }
