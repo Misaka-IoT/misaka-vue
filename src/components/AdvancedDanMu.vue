191a26027cdbb3f391d3d3846c97b1fd411878b4
z2bguoguo <88694479+z2bguoguo@users.noreply.github.com>
2023-06-10 20:04:03 +0800

    代码格式化

--- src/components/AdvancedDanMu.vue
+++ src/components/AdvancedDanMu.vue
@@ -1,65 +1,55 @@
 <template>
-  <a class="a" :style="{'color':color,'font-size':FontSize,'font-family':FontFamily}">{{ danmu }}</a>
+  <a
+    class="a"
+    :style="{ color: color, 'font-size': FontSize, 'font-family': FontFamily }"
+    >{{ danmu }}</a
+  >
 </template>
-  <script lang="ts">
+<script lang="ts">
 import { ElFormItem } from 'element-plus';
 
-  export default {
-    name: 'AdvancedDanMu',
-    props:{txt:String},
-    data() {
-      return {
-        danmu:'',
-        color:'#D0CFCF',
-        FontSize:'20px',
-        css:'',
-        FontFamily:'Georgia'
-      };
-    },
-    methods: {
-      AnalyzeDanMu(str:String)
-      {
-        if(str.charAt(0)=='#')
-        {
-          this.color=str.substring(0,7) 
-          this.AnalyzeDanMu(str.toString().substring(7))
-        }
-        else if(str.substring(0,6)=='[size]')
-        {
-          this.FontSize=str.substring(6,8)+'px'   
-          this.AnalyzeDanMu(str.toString().substring(8))
-        }
-        else if(str.substring(0,7)=='[font]{')
-        {
-          this.FontFamily=str.substring(7,str.indexOf('}'))
-          this.AnalyzeDanMu(str.toString().substring(str.indexOf('}')+1))
-        }
-        else if(str.charAt(0)=='{')//屎山+114514
-        {
-          this.css=str.substring(1,str.indexOf('}'))
-          this.AnalyzeDanMu(str.toString().substring(str.indexOf('}')+1))
-        }
-        else
-        {
-          this.danmu=str.toString()
-        }   
+export default {
+  name: 'AdvancedDanMu',
+  props: { txt: String },
+  data() {
+    return {
+      danmu: '',
+      color: '#D0CFCF',
+      FontSize: '20px',
+      css: '',
+      FontFamily: 'Georgia',
+    };
+  },
+  methods: {
+    AnalyzeDanMu(str: String) {
+      if (str.charAt(0) == '#') {
+        this.color = str.substring(0, 7);
+        this.AnalyzeDanMu(str.toString().substring(7));
+      } else if (str.substring(0, 6) == '[size]') {
+        this.FontSize = str.substring(6, 8) + 'px';
+        this.AnalyzeDanMu(str.toString().substring(8));
+      } else if (str.substring(0, 7) == '[font]{') {
+        this.FontFamily = str.substring(7, str.indexOf('}'));
+        this.AnalyzeDanMu(str.toString().substring(str.indexOf('}') + 1));
+      } else if (str.charAt(0) == '{') {
+        //屎山+114514
+        this.css = str.substring(1, str.indexOf('}'));
+        this.AnalyzeDanMu(str.toString().substring(str.indexOf('}') + 1));
+      } else {
+        this.danmu = str.toString();
       }
     },
-    created() {
-      if(this.txt!=undefined)
-      {
-        this.AnalyzeDanMu(this.txt)
-      }
-      
-    },
-
-    
-  };
+  },
+  created() {
+    if (this.txt != undefined) {
+      this.AnalyzeDanMu(this.txt);
+    }
+  },
+};
 </script>
 <style lang="scss" scoped>
-.a{
-  opacity:0.5;
-  font-weight:bold;
+.a {
+  opacity: 0.5;
+  font-weight: bold;
 }
 </style>
-  
