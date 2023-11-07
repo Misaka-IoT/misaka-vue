07d992a4466354e8b6a61f2eedfca5b2b8298d4a
longyu12345 <litong4576@gmail.com>
2023-11-07 17:19:30 +0800

    fix : fixed  comment board

--- src/views/DiscussView.vue
+++ src/views/DiscussView.vue
@@ -10,11 +10,18 @@ export default {
   mounted: function () {
     new Artalk({
       el: '.artalk-comments',
-      pageKey: `233`,
-      pageTitle: 'misaka-mikoto.jp-comment-server',
-      server: 'https://chat.mashiro.pub:23366',
-      site: 'lolimashiro-comment-sys',
+      // pageKey: `233`,
+      pageTitle: '爱我超电磁炮',
+      server: 'https://misaka-mikoto.cn/talk',
+      site: '爱我超电磁炮',
       // ...
+    });
+
+    Artalk.init({
+      avatarURLBuilder: (comment) => {
+        // 调用
+        return `https://cravatar.cn/avatar/${comment.email_encrypted}`;
+      },
     });
   },
 };
