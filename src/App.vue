551ef873aea69ac6b7acc2c82fa01b1c94d15ae3
shbwb <104608787+misaka-fans@users.noreply.github.com>
2024-01-14 15:57:33 +0800

    Enable Https

--- src/App.vue
+++ src/App.vue
@@ -84,11 +84,6 @@ import { Icon } from '@iconify/vue';
                   d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path>
               </svg>
             </div>
-            <div
-              class="md-source__repository md-source__repository--active"
-              style="width: 150%">
-              misaka-fans
-            </div>
           </a>
         </div>
       </template>
@@ -139,12 +134,12 @@ export default {
     }
 
     // 使用axios获取star和fork数量
-    axios
+  /*axios
       .get('https://api.github.com/repos/Misaka-IoT/misaka-vue/')
       .then((res) => {
         this.stargazersCount = res.data.stargazers_count;
         this.forksCount = res.data.forks_count;
-      });
+      });*/
   },
   data() {
     return {
@@ -153,7 +148,7 @@ export default {
       stargazersCount: 0,
       forksCount: 0,
       site: 'main',
-      cdnRootUrl: 'http://img-cdn-for-misaka.misaka-network.tk/',
+      cdnRootUrl: 'https://img-cdn-for-misaka.misaka-network.tk/',
       danmu: {
         danmus: [] as any,
         channels: 0,
