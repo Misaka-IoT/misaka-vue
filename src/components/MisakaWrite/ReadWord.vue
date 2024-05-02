813310e45380dd208bf022603866430995dfd505
z2bguoguo <88694479+z2bguoguo@users.noreply.github.com>
2024-05-02 19:22:11 +0800

    增加了点赞功能

---
+++ src/components/MisakaWrite/ReadWord.vue
@@ -0,0 +1,71 @@
+<template>
+<div v-if="on" class="Read-on">
+    标题：<input type="text" :value="title" style="border: none;border-radius: 5px;margin-bottom:10px;" readonly/><br>
+    正文：<textarea :value="text" style="border: none;border-radius: 5px;height: 300px;width:300px" readonly/><br>
+    {{ info }}
+</div>
+<div :onclick="off" v-if="on" style="z-index: 9;position: fixed; top: 0;left: 0;width: 100vw;height: 100vw;background-color: rgba(0, 0, 0, 0.5);">
+</div>
+</template>
+<script lang="ts">
+    import axios from 'axios';
+    export default {
+    name: 'ReadWord',
+    data() {
+        return {
+        text:'',
+        title:'',
+        info:'',
+        };
+    },
+    methods: {
+        off()
+        {
+            this.$emit("ReadSwich")
+        },
+        OnRead(id:string)
+        {
+            console.log(id);
+            
+            this.info='加载中'
+            axios
+            .get('https://write.z2bguoguo.cn/',{
+                headers: {mode: 'read',page:id},
+            })
+            .then((res) => { 
+                try {
+                    var j=res.data
+                    this.title=j.title
+                    this.text=j.text
+                    this.info='加载完成'
+                } 
+                catch (e) {
+                    this.info='加载失败'
+                }
+            });
+        }
+    },
+    props:['on'],
+    };
+</script>
+<style>
+.button {
+    position: absolute;
+    background-color:#b35900;
+    color: #fff;
+    border: .5px #993d00;
+    border-radius: 5px;
+    padding: 10px;
+    box-shadow:  2px 2px rgba(0, 0, 0, .7);
+}
+.button:hover {
+    background-color:#C66910;
+    box-shadow: none;
+    transition: 0.7s;
+}
+.button:focus {
+    outline-color: transparent;
+    outline-style:solid;
+    box-shadow: 0 0 0 4px #C66910;
+}
+</style>
--- src/views/MisakaWrite.vue
+++ src/views/MisakaWrite.vue
@@ -3,11 +3,12 @@ import { Icon } from '@iconify/vue';
 </script>
 <template>
   <ul class="WriteIndex">
-    <li class="WriteItem" v-for="(i, j)  in item" :key="i.id">
+    <li class="WriteItem" v-for="(i, j)  in item" :key="i.id" >
       <div style="height: 100px;">
-        <h1 style="width: 80%;float: left">{{ i.title }}</h1>
-        <button style="float: right; top:50%" class="icon-btn standard no-color">
-          <Icon icon="material-symbols:favorite" width="50px" height="50px" />
+        <h1 style="width: 80%;float: left ;user-select: none;height: 80%;" @click="ReadSwich(i.id)">{{ i.title }}</h1>
+        <h2 style="left: 80%;top: -54%;position: relative;width: fit-content;height: 100%;">{{ i.star }}</h2>
+        <button style="float: right;top:-54%" class="icon-btn standard no-color"  @click="OnStar(i.id)">
+          <Icon :icon="IsIcon(i.id)" width="50px" height="50px" />
         </button>
       </div>
     </li>
@@ -16,21 +17,77 @@ import { Icon } from '@iconify/vue';
     <Icon icon="material-symbols:upload" width="50px" height="50px" />
   </button>
   <Upload :on="OnUp" @UPSwich="UpSwich"></Upload>
+  <ReadWord  :on="OnRead" @ReadSwich="ReadSwich('')" ref="Read"></ReadWord>
 </template>
 <script lang="ts">
   import axios from 'axios';
 import Upload from '@/components/MisakaWrite/Upload.vue';
+import ReadWord from '@/components/MisakaWrite/ReadWord.vue';
 export default {
     data() {
       return {
         item:[{id:'',title:'',star:''}],
-        OnUp:false
+        OnUp:false,
+        OnRead:false,
+        dzlb:['']
       };
     },
     methods: {
       UpSwich()
       {
         this.OnUp=!this.OnUp
+      },
+      ReadSwich(id:string)
+      {
+        this.OnRead=!this.OnRead;
+        (this.$refs.Read as any).OnRead(id);
+      },
+      OnStar(id:string)
+      {
+        var logins=localStorage.getItem("Login");
+        if(logins!=null)
+        {
+            var l=JSON.parse(logins)
+            axios
+            .get('https://write.z2bguoguo.cn/',{
+                headers: {mode: 'dz',em:l?.em,token:l?.token ,page:id},
+            })
+            .then((res) => { 
+                var dt=res.data
+                if(dt=='good')
+                {
+                    alert('点赞成功')
+                }
+                else if(dt=='user')
+                {
+                  alert('用户错误')
+                }
+                else if(dt=='cf')
+                {
+                  alert('重复点赞')
+                }
+                else
+                {
+                  alert('点赞失败')
+                }
+            });
+        }
+        else
+        {
+          alert('没有登陆')
+        }
+        
+      },
+      IsIcon(id:string)
+      {
+        if(this.dzlb.length>0 && this.dzlb.findIndex((element) => element == id)>-1 )
+        {
+          return 'material-symbols:heart-check'
+        }
+        else
+        {
+          return 'material-symbols:favorite'
+        }
       }
     },
     created()
@@ -41,8 +98,28 @@ export default {
       .then((res) => {
         this.item=res.data;
       });
+      var logins=localStorage.getItem("Login");
+      if(logins!=null)
+      {
+          var l=JSON.parse(logins)
+          axios.get('https://write.z2bguoguo.cn/',{
+              headers: { mode:"dzlb",em:l?.em,token:l?.token  },
+          })
+          .then((res) => {
+            var r=res.data;
+            if(r!='user')
+            {
+              this.dzlb=res.data;
+            }
+            else
+            {
+              this.dzlb.splice(0);
+            }
+          });
+      }
+
     },
-    components:{Upload}
+    components:{Upload,ReadWord}
   };
 </script>
   
