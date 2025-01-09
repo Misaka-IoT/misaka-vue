85a7bcde8667bb004254690a78f123eb2d80f4f3
z2bguoguo <88694479+z2bguoguo@users.noreply.github.com>
2025-01-09 21:17:29 +0800

    优雅的让御坂ai更加智能

--- src/components/MisakaGpt.vue
+++ src/components/MisakaGpt.vue
@@ -10,22 +10,22 @@ export default {
   },
   methods: {
     GetReply(newmessage: string) {
-      this.messages.push({ role: 'user', content: newmessage });
+      this.messages.push({ role: 'user', parts:[{"text": newmessage}] });
       axios
         .post('https://misakagpt.z2bguoguo.cn/', JSON.stringify(this.messages))
         .then((res) => {
-          this.messages.push({ role: 'assistant', content: res.data.result });
-          alert(res.data.result);
-          console.log(res.data.result);
+          this.messages.push({ role: 'model',parts:[{"text": res.data.candidates[0].content.parts[0].text}]});
+          alert(res.data.candidates[0].content.parts[0].text);
+          console.log(res.data.candidates[0].content.parts[0].text);
         });
     },
     GetReply2(newmessage: string) {
-      this.messages.push({ role: 'user', content: newmessage });
+      this.messages.push({ role: 'user', parts:[{"text": newmessage}] });
       axios
         .post('https://misakagpt.z2bguoguo.cn/', JSON.stringify(this.messages))
-        .then((res) => {
-          this.messages.push({ role: 'assistant', content: res.data.result });
-          this.SendGPT(res.data.result);
+        .then((res) => {   
+          this.messages.push({ role: 'model', parts:[{"text": res.data.candidates[0].content.parts[0].text}] });
+          this.SendGPT(res.data.candidates[0].content.parts[0].text);
         });
     },
   },
